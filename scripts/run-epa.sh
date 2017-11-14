f79f4ade1ba2ab334420305aab8ad72044deb8e0
Fernando Ramirez <f.e.ramirez94@gmail.com>
2017-11-14 10:05:41 +0100

    improve run-epa script and add comments

--- scripts/run-epa.sh
+++ scripts/run-epa.sh
@@ -7,22 +7,27 @@ fileSearchString="*"$uuid"*"
 
 TREES_INDEX="trees/index.json"
 
+## find alignment file
 alignmentFile=$(jq '.trees[], .family_name' trees/index.json | jq --arg v $treeFamilyName 'select(.family_name==$v)' | jq -r '.alignment_file')
 
+## find tree file
 treeFile=$(jq '.trees[], .family_name' trees/index.json | jq --arg v $treeFamilyName 'select(.family_name==$v)' | jq -r '.tree_file')
 
+## find QS (query secuences) file
 qsFile=""
 for QS_FILE in $(find uploads/ -name $fileSearchString); do
 	qsFile=$QS_FILE
 done
 
+## create directory for results
 mkdir results/$uuid/
 
+## run EPA-ng algorithm
 epa-ng -s $alignmentFile -t $treeFile -q $qsFile -w results/$uuid/
 
+## remove unnecessary temporary generated files
 rm -rf results/$uuid/epa_info.log
 rm -rf results/$uuid/stat
 rm -rf results/$uuid/pepa.status
-
-#cat results/$uuid/epa_result.jplace | jq '.'
+rm -rf results/$uuid/*.bin
 
