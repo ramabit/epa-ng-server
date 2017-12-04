059c29be11737ca32d155966b4a7e08c4b4aeaa1
Fernando Ramirez <f.e.ramirez94@gmail.com>
2017-12-04 16:45:58 +0100

    improve script to generate png

--- scripts/graphic-from-results.sh
+++ scripts/graphic-from-results.sh
@@ -3,10 +3,14 @@
 uuid=$1
 
 ## Transform results in .jplace format to .newick format
-../genesis/bin/apps/./labelled_tree results/$uuid/epa_result.jplace results/$uuid/epa_result.nw
+if [ ! -f results/$uuid/epa_result.nw ]; then
+    ../genesis/bin/apps/./labelled_tree results/$uuid/epa_result.jplace results/$uuid/epa_result.nw
+fi
 
 ## activate environment in order to use ete3
 export PATH=~/anaconda_ete/bin:$PATH
 
 ## generate png image
+if [ ! -f results/$uuid/tree.png ]; then
 ete3 view -t results/$uuid/epa_result.nw --image results/$uuid/tree.png --face 'value:@name, color:auto()' --face 'value:@dist, pos:b-top, color:steelblue, size:8'
+fi
