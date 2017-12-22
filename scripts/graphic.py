96ceb838cec0ca90ff36320a443a1481b9883e4c
Fernando Ramirez <f.e.ramirez94@gmail.com>
2017-12-22 16:51:55 +0100

    starting to customize query branches

--- scripts/graphic.py
+++ scripts/graphic.py
@@ -1,17 +1,29 @@
-from ete3 import Tree
+from ete3 import Tree, NodeStyle
 import sys
 
 uuid=sys.argv[1]
 queryLabelsArray=sys.argv[2].split(",")
 
 newickFileName="results/" + uuid + "/epa_result.nw"
-imageResultFileName="results/" + uuid + "/tree.png
+imageResultFileName="results/" + uuid + "/tree.png"
 
 file = open(newickFileName, 'r')
 fileContent = file.read()
 file.close()
 
-t = Tree(fileContent)
-t.render(imageResultFileName, w=185, units="mm")
+tree = Tree(fileContent)
+
+ns1 = NodeStyle()
+ns1["fgcolor"] = "LightSteelBlue"
+
+for leaf in tree:
+	print(leaf.name)
+	if any(leaf.name in label for label in queryLabelsArray):
+		leaf.set_style(ns1)
+		print(leaf.name)
+		#leaf.add_features(color=colors.get("green"))
+        
+
+tree.render(imageResultFileName, w=185, units="mm")
 
 ## TODO Use queryLabelsArray to apply at those labels a different color in the graphic
