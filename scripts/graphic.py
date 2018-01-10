f99fa789548b85b55b1be7cb0e169554731ca086
Fernando Ramirez <f.e.ramirez94@gmail.com>
2018-01-10 15:24:39 +0100

    customize graphic returned

--- scripts/graphic.py
+++ scripts/graphic.py
@@ -1,4 +1,4 @@
-from ete3 import Tree, NodeStyle
+from ete3 import Tree, TreeStyle, NodeStyle, faces, AttrFace
 import sys
 
 uuid=sys.argv[1]
@@ -13,17 +13,37 @@ file.close()
 
 tree = Tree(fileContent)
 
-ns1 = NodeStyle()
-ns1["fgcolor"] = "LightSteelBlue"
+nsOriginal = NodeStyle()
+nsOriginal["fgcolor"] = "blue"
+
+nsNew = NodeStyle()
+nsNew["fgcolor"] = "red"
+nsNew["shape"] = "sphere"
+nsNew["size"] = 10
+nsNew["hz_line_type"] = 1
+nsNew["hz_line_color"] = "red"
+
+faceOriginal = AttrFace("name", fsize=5)
+faceNew = AttrFace("name", fsize=10)
+
+def newNode(leaf):
+	leaf.set_style(nsNew)
+	leaf.add_face(faceNew, column=0, position="branch-right")
+
+def originalNode(leaf):
+	leaf.set_style(nsOriginal)
+	leaf.add_face(faceOriginal, column=0, position="branch-right")
 
 for leaf in tree:
-	print(leaf.name)
 	if any(leaf.name in label for label in queryLabelsArray):
-		leaf.set_style(ns1)
-		print(leaf.name)
-		#leaf.add_features(color=colors.get("green"))
+		newNode(leaf)	
+	else:
+		originalNode(leaf)
         
+ts = TreeStyle()
+ts.show_leaf_name = False
 
-tree.render(imageResultFileName, w=185, units="mm")
+##tree.img_style["size"] = 5
+##tree.img_style["fgcolor"] = "LightSteelBlue"
 
-## TODO Use queryLabelsArray to apply at those labels a different color in the graphic
+tree.render(imageResultFileName, w=185, units="mm", tree_style=ts)
