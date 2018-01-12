9a234530b1c599811afd753445573cd7e6212219
Fernando Ramirez <f.e.ramirez94@gmail.com>
2018-01-12 15:00:42 +0100

    refactoring python graphical functions, add scripts for vertical, horizontal and circular views

---
+++ scripts/graphical/circular-tree.py
@@ -0,0 +1,18 @@
+from graphics import *
+
+tree = createTree()
+
+ts = TreeStyle()
+ts.show_leaf_name = False
+ts.show_scale = False
+ts.show_branch_length = True
+ts.margin_top = 5
+ts.margin_bottom = 5
+ts.margin_left = 5
+ts.margin_right = 5
+ts.mode = "c"
+
+## ts.mode = "c" // "c" for circular graphic, "r" for rectangular graphic
+## ts.orientation = 0 // 0 means up to down, 1 means down to up
+
+tree.render(imageResultFileName(), w=185, units="mm", tree_style=ts)
